2756bf312323b5318483fb1ecf8a1810f3c8096a
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-08-18 22:41:36 +0200

    javascript

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const wrapper = document.querySelector(".wrapper"),
    generateBtn = wrapper.querySelector(".form button");
 qrInput = wrapper.querySelector(".form input");
+qrImg = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click", () => {
    let qrValue = qrInput.value;
-   console.log(qrValue);
-
-   wrapper.classList.add("active");
+   if (!qrValue) return; // if the input is empty then return from here
+   generateBtn.innerText = "Generating QR Code...";
+   // API and passing the api  to qrImg
+   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+   qrImg.addEventListener("load", () => {
+      wrapper.classList.add("active");
+      generateBtn.innerText = "Generate QR code";
+   });
 });
